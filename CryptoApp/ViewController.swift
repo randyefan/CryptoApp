0e02a97fc08bf2201800d2f4e0ab47259a45da7e
Randy Efan <randy@mamiteam.com>
2021-02-05 23:01:48 +0900

    Initial Commit

---
+++ CryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by Randy Efan Jayaputra on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
